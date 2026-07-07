75978f5a5b09424386998ec7964310cd3a3e2b92
Khuong Vu <kvu@csusm.edu>
2026-07-07 11:53:52 -0700

    Initial commit

---
+++ public/assets/javascripts/csusm.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+        var pathname = window.location.pathname;
+        var archive_obj = window.location.href;
+        jQuery("#navigation .navbar #collapsemenu ul").after("<ul class='nav navbar-nav navbar-right'><li><a href='https://archives.csusm.edu'>Special Collections Home Page</a></li></ul>");
+        jQuery("#request_sub").replaceWith("<a href='https://archives.csusm.edu/request?ref_url=https://archivessearch.csusm.edu"+pathname+"' class='btn page_action request  btn-default' target='_blank'><i class='fa fa-bullhorn fa-3x'></i><br/>Request<span class='sr-only'>(opens in a new window)</span></a>");
+	// 07/03/26 Khuong: A number of lines were deleted, because they are no longer
+	// applicable; the UI element(s) have changed because of the data or because
+	// we are going with Lyrasis' search bar, footer, and so on, instead of ours.
+});
